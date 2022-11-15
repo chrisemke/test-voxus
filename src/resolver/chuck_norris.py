b8d7f12912dbaf21f49dd3f9393c3b76bde9f500
Christian G. Semke <chrisemke@protonmail.com>
2022-11-15 15:15:11 -0300

    fix(limit): the filter by list key 0 to limit

--- src/resolver/chuck_norris.py
+++ src/resolver/chuck_norris.py
@@ -2,7 +2,6 @@ from resolver.sanitize_resolver import Sanitizer
 from requests import get
 from utils.validator import validate_categories
 from urllib.parse import quote
-from itertools import islice
 
 
 def get_jokes(
@@ -11,6 +10,7 @@ def get_jokes(
 	limit: int = 5
 ) -> Sanitizer:
 	valid_categorie = validate_categories(category)
+
 	if valid_categorie:
 		response = get(
 			f'https://api.chucknorris.io/jokes/random?category={category}'
@@ -21,11 +21,12 @@ def get_jokes(
 		url_query = quote(free_text_search)
 		response = get(
 			f'https://api.chucknorris.io/jokes/search?query={url_query}'
-		).json()
-		if limit:
-			response = dict(islice(response.items(), limit))
+		).json()['result']
 
-		return list(map(call_sanitizer, response['result']))
+		if limit:
+			response = response[0:limit]
+
+		return list(map(call_sanitizer, response))
 
 
 def call_sanitizer(request_content: dict) -> Sanitizer:
